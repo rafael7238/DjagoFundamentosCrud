0a975869a74919e4a8651008e2eec740dfa62efa
Rafael Vasconcelos <rafael7238@hotmail.com>
2018-08-01 17:55:16 -0300

    first commit

---
+++ contas/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Categorias
+from .models import Transacao
+
+admin.site.register(Categorias)
+admin.site.register(Transacao)
+# Register your models here.
